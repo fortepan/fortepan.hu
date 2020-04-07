a80027b4749d1256f03cec726a60a613400f8fab
Richard Gazdik <richard@gazdik.name>
2020-04-07 21:30:12 +0200

    Update to a temporary thumbnail photo url in the config file

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  PHOTO_SOURCE: "https://fortepan.download/_photo/",
+  PHOTO_SOURCE: "https://eu.fortepan.download/_photo/",
   PHOTO_SOURCE_LARGE: "https://fortepan.download/_photo/download/fortepan_",
   THUMBNAILS_QUERY_LIMIT: 30,
   BREAKPOINT_PHONE_XS: 330,
