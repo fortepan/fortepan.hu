91a21b9e03b5abf6230a6975fcd7c9c653ef30e7
Richard Gazdik <richard@gazdik.name>
2020-07-22 18:51:44 +0200

    Update auth js

--- src/api/auth.js
+++ src/api/auth.js
@@ -13,10 +13,32 @@ const setSignedInStatus = isUserSignedIn => {
 }
 
 const signout = () => {
-  return new Promise(resolve => {
-    localStorage.removeItem("auth")
-    setSignedInStatus(false)
-    resolve()
+  return new Promise((resolve, reject) => {
+    const authData = JSON.parse(localStorage.getItem("auth"))
+
+    if (authData && authData.logout_token) {
+      const xmlHttp = new XMLHttpRequest()
+      xmlHttp.open("POST", `${AUTH_HOST}/user/logout?_format=json&token=${authData.logout_token}`, true)
+      xmlHttp.setRequestHeader("Content-Type", "application/json")
+      xmlHttp.withCredentials = true
+      xmlHttp.onload = () => {
+        if (xmlHttp.status === 204) {
+          setSignedInStatus(false)
+          resolve()
+        } else {
+          const respData = JSON.parse(xmlHttp.responseText)
+          reject(respData.message.replace(/(?:\r\n|\r|\n)/g, "<br />"))
+        }
+      }
+      xmlHttp.error = err => {
+        reject(err)
+      }
+      xmlHttp.send()
+    } else {
+      setSignedInStatus(false)
+      // eslint-disable-next-line prefer-promise-reject-errors
+      reject("User is not logged in")
+    }
   })
 }
 
@@ -57,31 +79,39 @@ const forgot = body => {
 
 const queryUser = () => {
   return new Promise((resolve, reject) => {
-    const authTokens = JSON.parse(localStorage.getItem("auth"))
-
-    if (authTokens && authTokens.current_user.mail) {
-      // return stored credentials
+    // if some of the user details are missing
+    const authData = JSON.parse(localStorage.getItem("auth"))
+    if (authData) {
+      // if auth data is present, we presume that the user is already logged in
       setSignedInStatus(true)
-      resolve(authTokens)
-    } else if (authTokens && authTokens.credentials) {
-      // if some of the user details are missing
+    }
+    // check if user data exists
+    if (authData && authData.current_user.uid) {
       const xmlHttp = new XMLHttpRequest()
-      xmlHttp.open("GET", `${AUTH_HOST}/jsonapi/user/user?filter[name]=${authTokens.current_user.name}`, true)
+      xmlHttp.open("GET", `${AUTH_HOST}/jsonapi/user/user?filter[uid]=${authData.current_user.uid}`, true)
       xmlHttp.setRequestHeader("Content-Type", "application/vnd.api+json")
       xmlHttp.setRequestHeader("Accept", "application/vnd.api+json")
-
+      xmlHttp.withCredentials = true
       xmlHttp.onload = () => {
         if (xmlHttp.status === 200) {
           const respData = JSON.parse(xmlHttp.responseText).data[0]
-          if (respData) {
-            authTokens.current_user.id = respData.id
-            authTokens.current_user.mail = respData.attributes.mail
-            authTokens.current_user.created = respData.attributes.created
-            authTokens.current_user.lang = respData.attributes.langcode
-            localStorage.setItem("auth", JSON.stringify(authTokens))
+          if (respData && respData.id) {
+            // if user id is present in the response
+            authData.current_user.id = respData.id
+            authData.current_user.mail = respData.attributes.mail
+            authData.current_user.created = respData.attributes.created
+            authData.current_user.lang = respData.attributes.langcode
+            localStorage.setItem("auth", JSON.stringify(authData))
+
+            // store the user details
+            setSignedInStatus(true)
+          } else {
+            // if user id is not present in the response then UI should switch to logged out state
+            // the cookie session might be expired in this case
+            setSignedInStatus(false)
           }
-          setSignedInStatus(true)
-          resolve(authTokens)
+
+          resolve(authData)
         } else {
           const respData = JSON.parse(xmlHttp.responseText)
           setSignedInStatus(false)
@@ -90,9 +120,7 @@ const queryUser = () => {
       }
       xmlHttp.send()
     } else {
-      setSignedInStatus(false)
-      // eslint-disable-next-line prefer-promise-reject-errors
-      reject("User is not logged in")
+      reject()
     }
   })
 }
@@ -135,13 +163,14 @@ const queryLists = () => {
 const signin = body => {
   return new Promise((resolve, reject) => {
     const xmlHttp = new XMLHttpRequest()
-    xmlHttp.open("POST", `${AUTH_HOST}/user/login?_format=json&token=logout_token`, true)
+    xmlHttp.open("POST", `${AUTH_HOST}/user/login?_format=json`, true)
     xmlHttp.setRequestHeader("Content-Type", "application/json")
     xmlHttp.withCredentials = true
     xmlHttp.onload = () => {
       if (xmlHttp.status === 200) {
         const respData = JSON.parse(xmlHttp.responseText)
-        respData.credentials = btoa(unescape(encodeURIComponent(`${respData.current_user.name}:${body.pass}`)))
+        // respData.credentials = btoa(unescape(encodeURIComponent(`${respData.current_user.name}:${body.pass}`)))
+        localStorage.setItem("auth", JSON.stringify(respData))
 
         // query user data ast the sign-in response doesn't contain all user info
         queryUser()
