e1be3c5e2fcbaf358e53d152faa40091e00a760d
Richard Gazdik <richard@gazdik.name>
2020-08-01 20:28:59 +0200

    Update auth

--- src/api/auth.js
+++ src/api/auth.js
@@ -77,55 +77,6 @@ const forgot = body => {
   })
 }
 
-const queryUser = () => {
-  return new Promise((resolve, reject) => {
-    // if some of the user details are missing
-    const authData = JSON.parse(localStorage.getItem("auth"))
-    if (authData) {
-      // if auth data is present, we presume that the user is already logged in
-      setLoginStatus(true)
-    }
-    // check if user data exists
-    if (authData && authData.current_user.uid) {
-      const xmlHttp = new XMLHttpRequest()
-      xmlHttp.open("GET", `${config.DRUPAL_HOST}/jsonapi/user/user?filter[uid]=${authData.current_user.uid}`, true)
-      xmlHttp.setRequestHeader("Content-Type", "application/vnd.api+json")
-      xmlHttp.setRequestHeader("Accept", "application/vnd.api+json")
-      xmlHttp.withCredentials = true
-      xmlHttp.onload = () => {
-        if (xmlHttp.status === 200) {
-          const respData = JSON.parse(xmlHttp.responseText).data[0]
-          if (respData && respData.id) {
-            // if user id is present in the response
-            authData.current_user.id = respData.id
-            authData.current_user.mail = respData.attributes.mail
-            authData.current_user.created = respData.attributes.created
-            authData.current_user.lang = respData.attributes.langcode
-            localStorage.setItem("auth", JSON.stringify(authData))
-
-            // store the user details
-            setLoginStatus(true)
-            resolve(authData)
-          } else {
-            // if user id is not present in the response then UI should switch to logged out state
-            // the cookie session might be expired in this case
-            setLoginStatus(false)
-            // eslint-disable-next-line prefer-promise-reject-errors
-            reject("The user has not been activated or is blocked.")
-          }
-        } else {
-          const respData = JSON.parse(xmlHttp.responseText)
-          setLoginStatus(false)
-          reject(respData.message)
-        }
-      }
-      xmlHttp.send()
-    } else {
-      reject()
-    }
-  })
-}
-
 const getUserStatus = () => {
   return new Promise((resolve, reject) => {
     const xmlHttp = new XMLHttpRequest()
@@ -134,13 +85,95 @@ const getUserStatus = () => {
     xmlHttp.withCredentials = true
     xmlHttp.onload = () => {
       if (xmlHttp.status === 200) {
-        resolve(xmlHttp.responseText)
+        if (xmlHttp.responseText === "1") {
+          console.log("User is logged in.")
+          resolve(true)
+        } else resolve(false)
       } else {
         const respData = JSON.parse(xmlHttp.responseText)
         reject(respData.message)
       }
     }
     xmlHttp.send()
+  })
+}
+
+const queryUser = () => {
+  return new Promise((resolve, reject) => {
+    // check user status
+    getUserStatus()
+      .then(resp => {
+        if (resp === true) {
+          // if user is logged in
+          console.log("queryUser: User is logged in")
+
+          // check localstorage auth data
+          const authData = JSON.parse(localStorage.getItem("auth")) || {}
+
+          // if some params are missing from the stored object
+          if (!authData.current_user || (authData.current_user && !authData.current_user.email)) {
+            // request user data
+            console.log("queryUser: Request user data")
+
+            const xmlHttp = new XMLHttpRequest()
+            xmlHttp.open("GET", `${config.DRUPAL_HOST}/user/login`, true)
+            xmlHttp.withCredentials = true
+            xmlHttp.send()
+            /*
+            
+            xmlHttp.setRequestHeader("Content-Type", "application/vnd.api+json")
+            xmlHttp.setRequestHeader("Accept", "application/vnd.api+json")
+            xmlHttp.withCredentials = true
+            xmlHttp.onload = () => {
+              console.log("queryUser: Use data loaded")
+              if (xmlHttp.status === 200) {
+                const respData = JSON.parse(xmlHttp.responseText).data[0]
+                console.log(respData)
+                // if user id is present in the response
+                if (respData && respData.id) {
+                  if (!authData.current_user) authData.current_user = {}
+                  authData.current_user.id = respData.id
+                  authData.current_user.name = respData.name
+                  authData.current_user.mail = respData.attributes.mail
+                  authData.current_user.created = respData.attributes.created
+                  authData.current_user.lang = respData.attributes.langcode
+                  localStorage.setItem("auth", JSON.stringify(authData))
+                  // store the user details
+                  setLoginStatus(true)
+                  resolve(authData)
+                } else {
+                  // if user id is not present in the response then UI should switch to logged out state
+                  setLoginStatus(false)
+                  reject()
+                }
+              } else {
+                const respData = JSON.parse(xmlHttp.responseText)
+                setLoginStatus(false)
+                reject(respData.message)
+              }
+            }
+
+            console.log("xmlHttp.send()")
+        
+             */
+          } else {
+            console.log("reject 1")
+            setLoginStatus(false)
+            reject()
+          }
+        } else {
+          // if user is logged out
+          console.log("reject 2")
+          setLoginStatus(false)
+          reject()
+        }
+      })
+      .catch(error => {
+        // there was a network issue during the call
+        setLoginStatus(false)
+        console.log("queryuser rejected")
+        reject(error)
+      })
   })
 }
 
@@ -176,9 +209,11 @@ const signin = body => {
         setTimeout(() => {
           queryUser()
             .then(resp => {
+              console.log("queryuser resolved")
               resolve(resp)
             })
             .catch(error => {
+              console.log("queryuser rejected")
               reject(error)
             })
         }, 1000)
