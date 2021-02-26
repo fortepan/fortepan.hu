dbd5a84b484f31346e49a6c21359ddc3ae8efba1
richardgazdik <richard.gazdik+github@gmail.com>
2021-02-26 09:19:17 +0100

    Fix: Fix donor labels in the english version of the detail page

--- src/api/search.js
+++ src/api/search.js
@@ -19,7 +19,7 @@ const transformResults = resp => {
       item.uuid = h.uuid
       item.created = h.created
       item.description = h.description
-      item.donor = l === "hu" ? h.adomanyozo_name : h.adomanyozo_en
+      item.donor = h.adomanyozo_name
       item.author = l === "hu" ? h.szerzo_name : h.szerzo_en
       item.tags = l === "hu" ? h.cimke_name : h.cimke_en
       item.country = l === "hu" ? h.orszag_name : h.orszag_en
