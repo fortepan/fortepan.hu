2cd0786377d4ba252d0d9f53c32cd3f5edcae05a
jepegrafik <jepegrafik@gmail.com>
2021-04-27 18:40:24 +0200

    Feature/lists: extending the features of the lists dialog

--- src/components/dialog/lists/lists-controller.js
+++ src/components/dialog/lists/lists-controller.js
@@ -1,11 +1,12 @@
 import { Controller } from "stimulus"
 import { trigger, lang } from "../../../js/utils"
-import { appState, selectedThumbnail } from "../../../js/app"
+import { appState } from "../../../js/app"
+import photoManager from "../../../js/photo-manager"
 import listsAPI from "../../../api/lists"
 
 export default class extends Controller {
   static get targets() {
-    return ["form", "select", "name", "submitButton"]
+    return ["addedTo", "addedToList", "form", "select", "name", "description", "submitButton"]
   }
 
   connect() {
@@ -21,7 +22,7 @@ export default class extends Controller {
       listID = await listsAPI.createList(this.formTarget.name.value)
     }
 
-    await listsAPI.addToList(selectedThumbnail.itemData.mid[0], listID)
+    await listsAPI.addToList(photoManager.getSelectedPhotoId(), listID)
 
     trigger("dialogLists:hide")
   }
@@ -40,22 +41,44 @@ export default class extends Controller {
 
   toggleInput() {
     this.nameTarget.classList.toggle("is-hidden", this.selectTarget.value !== "0")
+    this.descriptionTarget.classList.toggle("is-hidden", this.selectTarget.value !== "0")
   }
 
   async renderOptions() {
     const resp = await listsAPI.getLists()
-    let innerHTML = `<option value="0">${lang("list_new")}</option>`
+    let innerHTML = ""
     Object.keys(resp).forEach(key => {
       innerHTML += `<option value="${key}">${resp[key]}</option>`
     })
+    innerHTML += `<option value="0">${lang("list_new")}</option>`
     this.selectTarget.innerHTML = innerHTML
 
     return resp
   }
 
+  async getContainingLists(id) {
+    /*
+      TODO
+    */
+    /* const resp = await listsAPI.getContainingLists(id)
+
+    // list the lists that contains the image
+    let innerHTML = ""
+    Object.keys(resp).forEach(key => {
+      innerHTML += `<li><h5><a href="/">{ list name }</a></h5> (<a href="#">${lang("list_edit")}</a>)</li>`
+    })
+    this.addedToListTarget.innerHTML = innerHTML
+
+    this.addedToTarget.classList.toggle("is-visible", resp.length !== -1)
+
+    return resp */
+    return id
+  }
+
   async show() {
     if (appState("auth-signed-in")) {
       this.element.classList.add("is-visible")
+      await this.getContainingLists(photoManager.getSelectedPhotoId())
       await this.renderOptions()
       this.toggleInput()
     } else {
