c3a1cf64d7c9ffd492a2a7ee95d8778fa3cadb34
jepegrafik <jepegrafik@gmail.com>
2022-10-18 15:48:21 +0530

    GA4 tracking added

---
+++ src/js/google-analytics-4.js
@@ -0,0 +1,54 @@
+/**
+ * GA Helper
+ *
+ * You can trigger the following events from any components
+ * by using the trigger() method from utils
+ *
+ * trigger("analytics:trackPageview")
+ * trigger("analytics:trackEvent", {eventCategory, eventAction, eventLabel})
+ *
+ */
+
+const GA4 = {
+  init: () => {
+    // pollute window
+    window.dataLayer = window.dataLayer || []
+    function gtag(...args) {
+      window.dataLayer.push(args)
+    }
+    gtag("js", new Date())
+    gtag("config", "G-BQPYXSY6WP")
+
+    // inject GA script to document header
+    const a = document.createElement("script")
+    a.async = true
+    a.src = "https://www.googletagmanager.com/gtag/js?id=G-BQPYXSY6WP"
+    const s = document.getElementsByTagName("script")[0]
+    s.parentNode.insertBefore(a, s)
+  },
+  trackPageView: () => {
+    // GA4 tracks page view on browser history change automatically
+    // we'll not use the code below for now
+    /* if (window.dataLayer)
+      window.dataLayer.push("event", "page_view", {
+        page_location: document.location.pathname + document.location.search,
+      }) */
+  },
+  trackEvent: (c, a, l, v) => {
+    if (window.dataLayer)
+      window.dataLayer.push("event", a, {
+        event_category: c,
+        event_label: l,
+        event_value: v,
+      })
+  },
+}
+
+/** Custom event listeners */
+
+document.addEventListener("analytics:trackEvent", GA4.trackEvent)
+document.addEventListener("analytics:trackPageView", GA4.trackPageView)
+
+/** GA tracking is only allowed when users approve it with the cookie consent */
+
+document.addEventListener("cookieConsent:cookiesAllowed", GA4.init)
--- src/site.js
+++ src/site.js
@@ -6,6 +6,7 @@ import "./js/stimulus"
 
 // Init GA event handling
 import "./js/google-analytics"
+import "./js/google-analytics-4"
 
 // Redirects to old site urls (if needed)
 import "./js/redirects"
