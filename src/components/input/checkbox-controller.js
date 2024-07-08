d98c55b8179a719691e7dd19276a7afbeb5b7270
jepegrafik <jepegrafik@gmail.com>
2024-07-08 13:30:42 +0200

    Checkbox component: trigger change event on checkbox when changing it from js

--- src/components/input/checkbox-controller.js
+++ src/components/input/checkbox-controller.js
@@ -1,5 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
+import { trigger } from "../../js/utils"
+
 export default class extends Controller {
   static get targets() {
     return ["input", "label"]
@@ -7,5 +9,6 @@ export default class extends Controller {
 
   toggleCheck() {
     this.inputTarget.checked = !this.inputTarget.checked
+    trigger("change", null, this.inputTarget)
   }
 }
