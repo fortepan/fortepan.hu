6cf78fb1649e67baa4751706ca62418fd21fcb4c
jepegrafik <jepegrafik@gmail.com>
2024-09-30 11:46:11 +0200

    Removing generated files from the git repo

--- .tina/__generated__/client.js
+++
@@ -1,5 +0,0 @@
-import { createClient } from "tinacms/dist/client";
-import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: '7cf630f1ba919d1eeaa8ffb4200976a2c77e6453', queries });
-export default client;
-  
