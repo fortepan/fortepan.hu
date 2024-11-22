a05c60065fe5d369cc90b5d7567adcc35b0d352a
jepegrafik <jepegrafik@gmail.com>
2024-11-22 19:43:59 +0530

    Revert "Projects page content & admin update"
    This reverts commit 9b7fd3f4ed08eb791dc4c4abc21b84e45c24bbf7.

--- .tina/templates/projects.js
+++ .tina/templates/projects.js
@@ -1,16 +1,22 @@
 export default {
   fields: [
     {
+      type: "string",
+      name: "title",
+      label: "title",
+    },
+    {
+      type: "rich-text",
+      name: "body",
+      label: "Body of Document",
+      description: "This is the markdown body",
+      isBody: true,
+    },
+    {
       type: "object",
       name: "projects",
       label: "Projects",
       list: true,
-      ui: {
-        component: "group-list",
-        itemProps: item => ({
-          label: item.title,
-        }),
-      },
       fields: [
         {
           type: "string",
@@ -23,17 +29,23 @@ export default {
           label: "project_date",
         },
         {
-          type: "rich-text",
+          type: "string",
           name: "description",
           label: "description",
-        },
-        {
-          type: "rich-text",
-          name: "funding_info",
-          label: "funding_info",
+          ui: {
+            component: "textarea",
+          },
         },
         {
           type: "string",
+          name: "funding_info",
+          label: "funding_info",
+          ui: {
+            component: "textarea",
+          },
+        },
+        {
+          type: "image",
           name: "funding_logo",
           label: "funding_logo",
         },
