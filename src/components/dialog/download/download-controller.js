a0dc571f2890b962fc5aa8c0e0f841eb4aace578
jepegrafik <jepegrafik@gmail.com>
2025-11-11 12:56:39 +0700

    Download dialog: adding author to credits + option to copy the text on the clipboard

--- src/components/dialog/download/download-controller.js
+++ src/components/dialog/download/download-controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 import config from "../../../data/siteConfig"
-import { lang } from "../../../js/utils"
+import { copyToClipboard } from "../../../js/utils"
 import { appState } from "../../../js/app"
 import photoManager from "../../../js/photo-manager"
 import listManager from "../../../js/list-manager"
@@ -15,10 +15,12 @@ export default class extends Controller {
     this.element.classList.add("is-visible")
 
     this.photoData = appState("is-lists") ? listManager.getSelectedPhoto() : photoManager.getSelectedPhotoData()
-    this.contentTarget.innerHTML = lang("dialog_download").replace(
-      "$donor",
-      `<br><b>Fortepan / ${this.photoData.donor}</b>`
-    )
+    this.photoCredits = `Fortepan / ${this.photoData.donor}`
+    if (this.photoData.author) {
+      this.photoCredits += ` / ${this.photoData.author}`
+    }
+
+    this.contentTarget.innerHTML = this.photoCredits
   }
 
   hide() {
@@ -31,5 +33,11 @@ export default class extends Controller {
     const a = document.createElement("a")
     a.href = `${config().PHOTO_SOURCE_LARGE}${this.photoData.mid}.jpg`
     a.click()
+  }
+
+  onCopyClicked(e) {
+    if (e) e.preventDefault()
+
+    copyToClipboard(this.photoCredits)
   }
 }
