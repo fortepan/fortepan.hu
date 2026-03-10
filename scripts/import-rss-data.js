ac5928d9f4ef4710ae51bab901e16e3a2afdbe3d
jepegrafik <78535407+jepegrafik@users.noreply.github.com>
2026-03-10 14:53:01 +0700

    RSS import: proper error handling for connection timeout

--- scripts/import-rss-data.js
+++ scripts/import-rss-data.js
@@ -5,33 +5,55 @@ const parser = new Parser({
   customFields: {
     item: ["featured_image"],
   },
+  timeout: 60000,
 })
 
 ;(async () => {
   const rss = {}
+  let feed
 
-  let feed = await parser.parseURL("https://hetifortepan.capacenter.hu/feed/")
-  rss.hu = feed.items
+  try {
+    feed = await parser.parseURL("https://hetifortepan.capacenter.hu/feed/")
+    rss.hu = feed.items
+  } catch (err) {
+    console.error("Error fetching HU feed:", err.message)
+  }
 
-  feed = await parser.parseURL("https://hetifortepan.capacenter.hu/en/feed/")
-  rss.en = feed.items
+  // continue only if HU feed was fetched successfully
+  if (rss.hu) {
+    try {
+      feed = await parser.parseURL("https://hetifortepan.capacenter.hu/en/feed/")
+      rss.en = feed.items
+    } catch (err) {
+      console.error("Error fetching EN feed:", err.message)
+    }
+  }
 
-  Object.keys(rss).forEach(lang => {
-    rss[lang].forEach(item => {
-      if (item.featured_image && item.featured_image.includes("fortepan_")) {
-        const fileName = item.featured_image.split("fortepan_")[1]
+  if (rss.hu && rss.en) {
+    // write out a combined file only we have both feeds fetched successfully
+    Object.keys(rss).forEach(lang => {
+      rss[lang].forEach(item => {
+        if (item.featured_image && item.featured_image.includes("fortepan_")) {
+          const fileName = item.featured_image.split("fortepan_")[1]
 
-        if (fileName.includes("-") || fileName.includes(".")) {
-          const mid = fileName.includes("-") ? fileName.split("-")[0] : fileName.split(".")[0]
+          if (fileName.includes("-") || fileName.includes(".")) {
+            const mid = fileName.includes("-") ? fileName.split("-")[0] : fileName.split(".")[0]
 
-          // save the mid if it only contains numbers (then it's most probably a valid mid)
-          if (/^\d+$/.test(mid.trim())) item.featured_image_mid = mid
+            // save the mid if it only contains numbers (then it's most probably a valid mid)
+            if (/^\d+$/.test(mid.trim())) item.featured_image_mid = mid
+          }
         }
+      })
+    })
+
+    fs.writeFile(`src/data/rss.json`, JSON.stringify(rss), err => {
+      if (err) {
+        console.error("Error writing RSS file:", err)
+      } else {
+        console.log("RSS data successfully saved")
       }
     })
-  })
-
-  fs.writeFile(`src/data/rss.json`, JSON.stringify(rss), err => {
-    if (err) console.log("error", err)
-  })
+  } else {
+    console.error("Error: Could not fetch either HU and EN feeds, no file is written")
+  }
 })()
